553d9faec3eb21e14ca043f0c434d4b0e168b808
Lorena Suárez <30444782+lorelero@users.noreply.github.com>
2024-03-03 13:34:35 -0300

    Add files via upload

---
+++ assets/js/index.js
@@ -0,0 +1,10 @@
+let precioInicial = 400000;
+let cantidadInicial = 0;
+
+let precio = document.querySelector(".precio");
+let cantidad =document.querySelector(".cantidad");
+let total = document.querySelector(".total");
+
+precio.innerHTML = precioInicial;
+cantidad.innerHTML = cantidadInicial;
+total.innerHTML = precioInicial;
